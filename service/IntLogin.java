2353ff85aacd3b1947fffa2b350e2fea0d094757
Kiran0601 <55634005+Kiran0601@users.noreply.github.com>
2022-01-29 12:32:03 +0530

    Delete service directory

--- service/IntLogin.java
+++
@@ -1,9 +0,0 @@
-package com.revature.ers.service;
-
-
-public interface IntLogin {
-	public void login();
-	public void logout();
-
-
-}
--- service/IntManager.java
+++
@@ -1,9 +0,0 @@
-package com.revature.ers.service;
-
-public interface IntManager {
-
-    public void ManagerResolvedRequest();
-    public void ManagerPendingRequest();
-	public void view();
-	
-}
